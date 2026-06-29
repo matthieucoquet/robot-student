4ce84808f974d3fd6fe37b050dcdfdd491ec8ecc
Matthieu Coquet <matthieu.coquet@outlook.com>
2026-06-29 23:32:37 +0200

    Add first version of running normalizer

---
+++ src/robot_student/model/normalizer.py
@@ -0,0 +1,37 @@
+import torch
+from torch import nn
+
+
+class RunningNormalization(nn.Module):
+    def __init__(
+        self, shape: int | tuple[int, ...] | torch.Size, min_std: float = 1e-4, clip: float | None = None, device=None, dtype=None
+    ):
+        super().__init__()
+
+        self._clip = clip
+        self._min_var = min_std * min_std
+
+        self.register_buffer("_count", torch.tensor(0, device=device, dtype=torch.long))
+        self.register_buffer("_mean", torch.zeros(self._shape, device=device, dtype=dtype))
+        self.register_buffer("_var", torch.ones(self._shape, device=device, dtype=dtype))
+        self.register_buffer("_std", torch.ones(self._shape, device=device, dtype=dtype))
+
+    @torch.no_grad()
+    def update(self, x: torch.Tensor):
+        count_added = x.shape[0]
+        var_added, mean_added = torch.var_mean(x, dim=0, correction=0)
+
+        total_count = self._count + count_added
+        rate = count_added / total_count
+
+        mean_delta = mean_added - self._mean
+        self._count.copy_(total_count)
+        self._mean += rate * mean_delta
+        self._var += rate * (var_added - self._var + mean_delta * (mean_added - self._mean))
+        self._std.copy_(self._var).clamp_(min=self._min_var).sqrt_()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = (x - self._mean) / self._std
+        if self._clip is not None:
+            x = torch.clamp(x, -self._clip, self._clip)
+        return x
